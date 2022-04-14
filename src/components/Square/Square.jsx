b68c02dd3276b46c99b49cbbcde4a3bde9066a80
Wilmer Pocaterra <malgein17@gmail.comm>
2022-04-14 00:58:23 -0400

    first commit

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import classNames from 'classnames'
+import './Square.css'
+
+
+const Square = ({value, onClick, turn, winner}) => {
+
+  const handleClick = () => {
+    (turn !== null && value ===null) && onClick()
+  }
+
+  let squareClass = classNames ({
+    square: true,
+    [`square--${value}`]: value !== null,
+    winner: winner,
+  })
+
+  return (
+    <div className={squareClass} onClick={() => handleClick()}>
+
+    </div>
+  )
+}
+
+export default Square
